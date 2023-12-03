ab6e71c81d0c093fe367af3cd8f5ddaa406c2cb7
heinkhantzaw <hein@engineerforce.io>
2023-12-03 12:14:53 +0000

    update fetch for searching and sorting

--- main.js
+++ main.js
@@ -1,29 +1,49 @@
-new Vue({
-    el: "#app",
-    data: {
+const app = new Vue({
+    el: "#app", data: {
         title: "Lessons",
         descriptions: "You can choose the lessons here:",
+        baseURL: "http://localhost:3000/api/",
         lessons: [],
+        images: [],
         cart: [],
         isProductPage: true,
         searchQuery: "",
         sortBy: "Subject",
-        sortOrder: 1, // ascending
+        sortOrder: "asc",
         name: "",
         phoneNumber: "",
         isNameValid: true,
         isNumberValid: true,
     },
-    beforeMount() {
+
+    created: function () {
         this.fetchLessons();
+    },
+    watch: {
+        searchQuery: function () {
+            this.fetchAndFilterLessons();
+        },
+        sortBy: function () {
+            this.fetchAndFilterLessons();
+        },
+        sortOrder: function () {
+            this.fetchAndFilterLessons();
+        }
     },
     methods: {
         fetchLessons: async function () {
-            const response = await fetch("http://localhost:3000/api/lessons");
+            const response = await fetch(this.baseURL + "lessons");
             this.lessons = await response.json();
+            this.lessons.map(async (lesson) => {
+                this.images.push({
+                    Subject: lesson.Subject,
+                    URL: this.baseURL + "images/" + lesson.Subject + ".jpg"
+                });
+            });
         },
-        fetchImages: async function () {
-            const response = await fetch("http://localhost:3000/api/images");
+        fetchAndFilterLessons: async function () {
+          const response = await fetch(`${this.baseURL}lessons/filter?search=${this.searchQuery}&sortBy=${this.sortBy}&sortOrder=${this.sortOrder}`);
+          this.lessons = await response.json();
         },
         addToCart: function (lesson) {
             if (lesson.Spaces > 0) {
@@ -33,8 +53,7 @@ new Vue({
                     this.cart[lessonIndex].amount++;
                 } else {
                     this.cart.push({
-                        lesson: lesson,
-                        amount: 1
+                        lesson: lesson, amount: 1
                     });
                 }
             }
@@ -61,67 +80,52 @@ new Vue({
         },
         submitOrder: async function () {
             const finalCart = this.cart.map(item => ({
-                "_id": item.lesson._id,
-                "Subject": item.lesson.Subject,
-                "amount": item.amount
+                "_id": item.lesson._id, "Subject": item.lesson.Subject, "amount": item.amount
             }));
             const updatedLessons = this.cart.map(item => ({
-                "_id": item.lesson._id,
-                "Spaces": item.lesson.Spaces
+                "_id": item.lesson._id, "Spaces": item.lesson.Spaces
             }));
             const order = {
-                name: this.name,
-                phoneNumber: this.phoneNumber,
-                cart: finalCart
+                name: this.name, phoneNumber: this.phoneNumber, cart: finalCart
             };
-            fetch("http://localhost:3000/api/order", {
-                method: "POST",
-                headers: {
+            fetch(this.baseURL + "order", {
+                method: "POST", headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(order)
             }).then(async (res) => {
-                    const result = await res.json();
-
-                    if (result.acknowledged) {
-                        updatedLessons.map(async (lesson) => {
-                            await fetch(`http://localhost:3000/api/lessons/${lesson._id}`, {
-                                method: "PUT",
-                                headers: {
-                                    "Content-Type": "application/json"
-                                },
-                                body: JSON.stringify(lesson)
-                            });
-                        })
-                    }
+                const result = await res.json();
+                if (result.acknowledged) {
+                    updatedLessons.map(async (lesson) => {
+                        await fetch(`http://localhost:3000/api/lessons/${lesson._id}`, {
+                            method: "PUT", headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify(lesson)
+                        });
+                    })
                 }
-            );
+            });
         },
-        backToHome:
-
-            function (modalClose) {
-                // reset everything when user goes back home
-                this.isProductPage = true;
-                this.isNameValid = true;
-                this.isNumberValid = true;
-                this.name = "";
-                this.phoneNumber = "";
-                if (modalClose === true) {
-                    this.clearCheckoutForm();
-                }
+        backToHome: function (modalClose) {
+            // reset everything when user goes back home
+            this.isProductPage = true;
+            this.isNameValid = true;
+            this.isNumberValid = true;
+            this.name = "";
+            this.phoneNumber = "";
+            if (modalClose === true) {
+                this.clearCheckoutForm();
             }
-
-        ,
+        },
         validateName: function () {
             const nameRegex = /^[A-Za-z\s]+$/; // only letters and spaces
             this.isNameValid = nameRegex.test(this.name)
-        }
-        ,
+        },
         validateNumber: function () {
             let phoneRegex = /^[0-9]+$/; // only numbers
             this.isNumberValid = phoneRegex.test(this.phoneNumber);
-        }
-        ,
+        },
         clearCheckoutForm: function () {
             this.cart = [];
             this.name = "";
@@ -132,23 +136,18 @@ new Vue({
         cartItemCount: function () {
             return this.cart.length;
         },
-        displayedLessons: function () {
-            let result = this.lessons;
-            if (this.searchQuery !== "")
-                result = this.lessons.filter(lesson => {
-                    return lesson.Subject.toLowerCase().includes(this.searchQuery.toLowerCase()) || lesson.Location.toLowerCase().includes(this.searchQuery.toLowerCase());
-                });
-            return result.sort((a, b) => {
-                return this.sortOrder * (a[this.sortBy] > b[this.sortBy] ? 1 : -1); // ascending sort order
-            })
-        },
         isFormValid: function () {
             return this.name !== "" && this.isNameValid && this.phoneNumber !== "" && this.isNumberValid && this.cart.length > 0;
         },
         totalPrice: function () {
             return this.cart.reduce((total, item) => {
-                return total + item.lesson.Price * item.amount;
-            }, 0);
+                    return total + item.lesson.Price * item.amount;
+                },
+                0);
         }
     }
 });
+
+// fetch(this.baseURL + "lessons/filter?search=" + this.searchQuery + "&sortBy=" + this.sortBy + "&sortOrder=" + this.sortOrder).then(async (res) => {
+//     this.lessons = await res.json();
+// });
